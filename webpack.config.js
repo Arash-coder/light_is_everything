88c7ab7327e0460653afd81e5c7f37962cb9c536
mostafanadi <dev.mostafanadi@gmail.com>
2023-12-15 16:24:35 +0330

    fix loading fonts

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
-
 module.exports = {
-    devServer: {
-        headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-        },
-    },
+  devServer: {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers':
+        'X-Requested-With, content-type, Authorization'
+    }
+  }
 };
