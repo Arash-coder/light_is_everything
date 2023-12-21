aa3006b2f593aebe0fdfbe69385f9835dcde7902
mostafanadi <dev.mostafanadi@gmail.com>
2023-12-21 13:54:24 +0330

    add dashboard layout

---
+++ src/app/[locale]/(withdashboardlayout)/layout.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type Props = {
+  children: any
+}
+
+const Dashboardlayout = ({ children }: Props) => {
+  return (
+    <>
+      <div>Dashboardlayout</div>
+      {children}
+    </>
+  )
+}
+
+export default Dashboardlayout
