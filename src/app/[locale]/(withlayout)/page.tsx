d49699f749438e4382e9ceadaefb883fa73ea57b
Arash Barghash <arash.barghash138014@gmail.com>
2023-12-28 13:53:03 +0330

    change base url

--- src/app/[locale]/(withlayout)/page.tsx
+++ src/app/[locale]/(withlayout)/page.tsx
@@ -2,10 +2,13 @@ import HomePage from '@/components/pages/homePage';
 import Axios from '@/services/configAxios';
 import URLS from '@/services/urls';
 import { landingData } from '@/types/landing';
+import axios from 'axios';
 
 export default async function Home() {
   try {
-    const data: { data: landingData } = await Axios.get(URLS.landing);
+    const data: { data: landingData } = await axios.get(
+      'http://ight-backend-light_backend_web/landing/info'
+    );
 
     console.log('data', data);
     console.log('data.data', data.data);
