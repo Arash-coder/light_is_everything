501bf102f1154238458f1818d2cb2587a954502f
Arash Barghash <arash.barghash138014@gmail.com>
2023-12-28 12:34:45 +0330

    add log again

--- src/app/[locale]/(withlayout)/page.tsx
+++ src/app/[locale]/(withlayout)/page.tsx
@@ -4,7 +4,20 @@ import URLS from '@/services/urls';
 import { landingData } from '@/types/landing';
 
 export default async function Home() {
-  const data: { data: landingData } = await Axios.get(URLS.landing);
+  try {
+    const data: { data: landingData } = await Axios.get(URLS.landing);
 
-  return <HomePage data={data.data} />;
+    console.log('data', data);
+    console.log('data.data', data.data);
+
+    return <HomePage data={data.data} />;
+  } catch (error: any) {
+    console.log('err', error);
+    console.log('err2', error.message);
+    return (
+      <>
+        <div>error</div>
+      </>
+    );
+  }
 }
