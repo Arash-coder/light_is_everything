a9ec35b7c65d9a171ce845a0e77a007c9b883841
Arash Barghash <arash.barghash138014@gmail.com>
2023-12-28 14:23:14 +0330

    test fetch

--- src/app/[locale]/(withlayout)/page.tsx
+++ src/app/[locale]/(withlayout)/page.tsx
@@ -5,12 +5,16 @@ import { landingData } from '@/types/landing';
 
 export default async function Home() {
   try {
-    const data: { data: landingData } = await Axios.get(URLS.landing);
+    // const data: { data: landingData } = await Axios.get(URLS.landing);
+
+    const res = await fetch('https://api.lightiseverything.ir/landing/info');
+
+    const data = await res.json();
 
     console.log('data', data);
     console.log('data.data', data.data);
 
-    return <HomePage data={data.data} />;
+    return <HomePage data={data} />;
   } catch (error: any) {
     console.log('err', error);
     console.log('err2', error.message);
